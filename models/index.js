c83c33338121dd39f85e96c6d19208b1d617ff2b
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-11-12 17:46:17 +0900

    feat: #3 company_auth 미들웨어 파일 생성 22-11-12 17:45 (김민섭)

--- models/index.js
+++ models/index.js
@@ -3,10 +3,18 @@ const DB_NAME = process.env.DB_HOST;
 const mongoose = require("mongoose");
 
 const connect = () => {
-  mongoose.connect(DB_NAME, (error) => {
-    if (error) console.log("Mongo DB Connect Error");
-    else console.log("몽고db에 성공적으로 연결하였습니다.");
-  });
+  mongoose.connect(
+    DB_NAME,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "DMMHN",
+    },
+    (error) => {
+      if (error) console.log("Mongo DB Connect Error");
+      else console.log("몽고db에 성공적으로 연결하였습니다.");
+    }
+  );
 };
 
 module.exports = connect;
