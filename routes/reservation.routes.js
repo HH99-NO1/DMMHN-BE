6865e82dd23f72cd53098bde628447715f191709
Damiyd <dpekfa144@gmail.com>
2022-11-12 16:19:08 +0900

    feat: #5 기업 회원 면접 예약 구현중 22-11-12 16:19(최예닮)

--- routes/reservation.routes.js
+++ routes/reservation.routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authCompanyMiddleware = require('../middlewares/auth_company_middleware');
 
 const ReservationController = require('../controller/reservation.controller');
 const reservationController = new ReservationController();
 
-router.post("/reservation", authCompanyMiddleware, reservationController.postReservation)
+router.post("/reservation", reservationController.postReservation)
 
 module.exports = router;
