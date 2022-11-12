57500963c9f876f76332166028585f842f51e01d
minyoung <bree.j117@gmail.com>
2022-11-12 17:08:25 +0900

    feat: #2 회원가입 1차구현 22-11-12 17:08(장민영)

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.EXPRESS_PORT || 3000;
 const cors = require("cors");
-const connect = require("./models");
+const connect = require("./models/index");
 connect();
 
 const morgan = require("morgan");
 const routes = require("./routes/index.routes");
-
+app.use(express.json())
 app.use(
   cors({
     origin: "*",
   })
 );
-app.use(express.json());
+
 app.use(morgan("dev"));
 
 app.use("/", routes);
