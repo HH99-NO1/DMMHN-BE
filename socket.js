5f2610a6323cf3c004a3fab85b9e7c388949c823
Damiyd <dpekfa144@gmail.com>
2022-11-22 15:45:57 +0900

    feat: #12 webRTC 기본뼈대 추가 2차 22-11-22 15:45(최예닮)

--- socket.js
+++ socket.js
@@ -1,36 +1,34 @@
-const app = require('./app')
-const http = require('http')
+const app = require("./app");
+const http = require("http");
 
-const server = http.createServer(app)
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        credentials: true,
-    },
-})
-
-    io.on('connection', (socket) => {
-
-    socket.on('join_room', async () => {
-        socket.join()
-    })
-
-    socket.on('ice', () => {
-        socket.to().emit('ice', )
-    })
-
-    socket.on('offer', () => {
-        socket.to().emit('offer',)
-    })
-
-    socket.on('answer', () => {
-        socket.to().emit('answer',)
-    })
-
-    socket.on('disconnecting', async () => {
-
-    });
-
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
 });
 
-module.exports = { server }
+const MAXIMUM = 2;
+
+io.on("connection", (socket) => {
+  socket.on("join_room", async () => {
+    socket.join();
+  });
+
+  socket.on("ice", () => {
+    socket.to().emit("ice");
+  });
+
+  socket.on("offer", () => {
+    socket.to().emit("offer");
+  });
+
+  socket.on("answer", () => {
+    socket.to().emit("answer");
+  });
+
+  socket.on("disconnecting", async () => {});
+});
+
+module.exports = { server };
