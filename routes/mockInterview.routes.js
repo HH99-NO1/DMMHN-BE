6922735ccc81145362a6bf3b1d474a7229de6eea
chamchimayo <chamchi.maayo@gmail.com>
2022-11-24 15:07:01 +0900

    chore: #13 모의면접 질문 랜덤 선별 API 요청 GET -> POST로 변경 22-11-24 15:06(백지영)

--- routes/mockInterview.routes.js
+++ routes/mockInterview.routes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/auth_middleware");
 const MockInterviewController = require("../controller/mockInterview.controller");
 const mockInterviewController = new MockInterviewController();
 
-router.post("/", mockInterviewController.createQuestions);
-router.get("/", authMiddleware, mockInterviewController.getRandomQuestions);
+router.post("/createQuestions", mockInterviewController.createQuestions);
+router.post("/", authMiddleware, mockInterviewController.getRandomQuestions);
 
 module.exports = router;
