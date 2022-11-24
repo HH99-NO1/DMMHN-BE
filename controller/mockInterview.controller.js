ba813d95b32b34c5ad7fc7747521eae7406eed85
chamchimayo <chamchi.maayo@gmail.com>
2022-11-24 14:39:15 +0900

    chore: #13 모의면접 질문 랜덤 선별  로그 확인중 22-11-24 14:39(백지영)

--- controller/mockInterview.controller.js
+++ controller/mockInterview.controller.js
@@ -5,20 +5,20 @@ class MockInterviewController {
 
   createQuestions = async (req, res, next) => {
     const { category, question } = req.body;
-    console.log("***controller", req);
-    console.log("***controller req.body", req.body);
     try {
-        await this.mockInterviewService.createQuestions(category, question);
-
-        res.status(201).send("면접 질문을 생성하였습니다.");
+      await this.mockInterviewService.createQuestions(category, question);
+      
+      res.status(201).send("면접 질문을 생성하였습니다.");
     } catch (err) {
-        res.status(400).send(err.message);
+      res.status(400).send(err.message);
     }
   };
-
+  
   getRandomQuestions = async (req, res, next) => {
     const { category, number } = req.body;
-
+    console.log("***controller", req);
+    console.log("***controller req.body", req.body);
+    
     try {
       const data = await this.mockInterviewService.getRandomQuestions(category, number);
 
