bd70c8b6009d7e471f64577ce94d5e861604a818
chamchimayo <chamchi.maayo@gmail.com>
2022-11-25 10:35:23 +0900

    fix: 모든 Schema Date 대한민국 표준시로 들어가게끔 수정 22-11-25 10:35(백지영)

--- models/reservation.js
+++ models/reservation.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const curr = new Date();
+const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+const kr_curr = new Date(utc + KR_TIME_DIFF);
 
 // required: 무조건 필요한지
 const reservationSchema = new mongoose.Schema({
@@ -52,8 +56,8 @@ const reservationSchema = new mongoose.Schema({
   urlexpirated: {
     type: Boolean,
   },
-  createdAt: { type: String, default: new Date() },
-  updatedAt: { type: String, default: new Date() },
+  createdAt: { type: String, default: kr_curr },
+  updatedAt: { type: String, default: kr_curr },
   
 });
 
