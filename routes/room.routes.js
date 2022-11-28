0915d159d94b5800478b441c6eddff7dd137d3f8
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-11-28 19:52:47 +0900

    webRTC 방 입장 기능 1차 구현

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const auth_middleware = require("../middleware/auth_middleware");
 const router = express.Router();
 const Room = require("../models/room");
 
-router.post(":roomName", async (req, res) => {
+router.post(":roomName", auth_middleware, async (req, res) => {
   const { roomName } = req.params;
+  const { memberEmail } = res.locals.members;
   await Room.create({ roomName });
   res.status(201).send({ message: "방 생성 성공" });
+});
+
+router.get(":roomName", auth_middleware, async (req, res) => {
+  const { roomName } = req.params;
+  const memberEmail = res.locals.members;
+  res.status(200).json({ data: memberEmail, message: "방 입장 성공" });
 });
 
 module.exports = router;
