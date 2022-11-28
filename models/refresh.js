14545e83a2e95a078deb78cd008111619c3097fc
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-11-29 01:37:57 +0900

    #3 refreshToken DB에 저장 구현 22-11-29 01:37 (김민섭)

--- models/refresh.js
+++ models/refresh.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
-const curr = new Date();
-const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
-const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
-const kr_curr = new Date(utc + KR_TIME_DIFF);
+// const curr = new Date();
+// const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+// const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+// const kr_curr = new Date(utc + KR_TIME_DIFF);
 
 // required: 무조건 필요한지
 const refreshTokenSchema = new mongoose.Schema({
   refreshToken: {
     type: String,
   },
-  expiratedAt: { type: String, default: kr_curr },
-  updatedAt: { type: String, default: kr_curr },
+  createdAt: { type: Date, default: Date.now, expires: "2m" },
+  updatedAt: { type: Date },
 });
 
 module.exports = mongoose.model("refreshToken", refreshTokenSchema);
