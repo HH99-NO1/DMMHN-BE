408270f8f2a4484bf734eaa3409838a7321baeaf
Damiyd <dpekfa144@gmail.com>
2022-11-28 14:57:39 +0900

    feat: #12 db저장 없이 토큰값 받아오고 join까지 되는지 테스트 22-11-28 14:58(최예닮)

--- socket.js
+++ socket.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 const http = require("http");
+const authMiddleware = require("./middleware/auth_middleware");
+const Members = require("./models/members");
+const Room = require("./models/room");
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
@@ -18,83 +21,96 @@ let socketRoom = {};
 
 const MAXIMUM = 2;
 
-io.on("connection", (socket) => {
-  console.log(socket.id, "connection");
-  socket.on("join_room", (data) => {
-    console.log("join_room: ", data);
+app.get("/room/:roomName", authMiddleware, async (req, res) => {
+  const { roomName } = req.params;
+  const { memberEmail } = res.locals.members;
 
-    // 방이 기존에 생성되어 있다면
-    if (users[data.room]) {
-      console.log("27줄", users[data.room]);
+  // const findMembers = await Members.findOne({ memberEmail });
+  // console.log("#####findmember", findMembers);
 
-      // 현재 입장하려는 방에 있는 인원수
-      const currentRoomLength = users[data.room].length;
-      if (currentRoomLength === MAXIMUM) {
-        // 인원수가 꽉 찼다면 돌아갑니다.
-        socket.to(socket.id).emit("room_full");
-        return;
+  // const createRoom = await Room.create({
+  //   roomName,
+  //   user: memberEmail,
+  // });
+  // console.log("dbdbdb", createRoom);
+
+  io.on("connection", (socket) => {
+    console.log(memberEmail, "connection");
+    socket.on("join_room", async () => {
+      console.log("join_room: ", roomName,memberEmail);
+      // 방이 기존에 생성되어 있다면
+      // if (findRoom) {
+      //   console.log("27줄", findRoom);
+
+      //   // 현재 입장하려는 방에 있는 인원수
+      //   const numOfPeopleInRoom = findRoom.user.length;
+      //   if (numOfPeopleInRoom === MAXIMUM) {
+      //     // 인원수가 꽉 찼다면 돌아갑니다.
+      //     socket.to(user).emit("room_full");
+      //     return;
+      //   }
+
+      //   // 여분의 자리가 있다면 해당 방 배열에 추가해줍니다.
+      //   users[data.room] = [...users[data.room], { id: socket.id }];
+      // } else {
+      //   // 방이 존재하지 않다면 값을 생성하고 추가해줍시다.
+      //   users[data.room] = [{ id: socket.id }];
+      // }
+      // socketRoom[socket.id] = data.room;
+
+      // 입장
+      socket.join(roomName);
+      console.log("입장 !!!!", roomName);
+
+      // 입장하기 전 해당 방의 다른 유저들이 있는지 확인하고
+      // 다른 유저가 있었다면 offer-answer을 위해 알려줍니다.
+      const others = users[data.room].filter((user) => user.id !== socket.id);
+      if (others.length) {
+        io.sockets.to(socket.id).emit("all_users", others);
       }
+    });
 
-      // 여분의 자리가 있다면 해당 방 배열에 추가해줍니다.
-      users[data.room] = [...users[data.room], { id: socket.id }];
-    } else {
-      // 방이 존재하지 않다면 값을 생성하고 추가해줍시다.
-      users[data.room] = [{ id: socket.id }];
-    }
-    socketRoom[socket.id] = data.room;
+    socket.on("offer", (sdp, roomName) => {
+      console.log("offer: ", roomName);
+      // offer를 전달받고 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit("getOffer", sdp);
+    });
 
-    // 입장
-    socket.join(data.room);
-    console.log("입장 !!!!", data.room);
+    socket.on("answer", (sdp, roomName) => {
+      console.log("answer: ", roomName);
+      // answer를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit("getAnswer", sdp);
+    });
 
-    // 입장하기 전 해당 방의 다른 유저들이 있는지 확인하고
-    // 다른 유저가 있었다면 offer-answer을 위해 알려줍니다.
-    const others = users[data.room].filter((user) => user.id !== socket.id);
-    if (others.length) {
-      io.sockets.to(socket.id).emit("all_users", others);
-    }
-  });
+    socket.on("candidate", (candidate, roomName) => {
+      console.log("candidate :", roomName);
+      // candidate를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit("getCandidate", candidate);
+    });
 
-  socket.on("offer", (sdp, roomName) => {
-    console.log("offer: ", roomName);
-    // offer를 전달받고 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getOffer", sdp);
-  });
-
-  socket.on("answer", (sdp, roomName) => {
-    console.log("answer: ", roomName);
-    // answer를 전달받고 방의 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getAnswer", sdp);
-  });
-
-  socket.on("candidate", (candidate, roomName) => {
-    console.log("candidate :", roomName);
-    // candidate를 전달받고 방의 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getCandidate", candidate);
-  });
-
-  socket.on("disconnect", () => {
-    // const roomID = socketRoom[socket.id];
-    // console.log("roomID: ", roomID);
-    // socket.leave(roomID);
-    // socket.to(roomID).emit("leave", socket.id);
-    // 방을 나가게 된다면 socketRoom과 users의 정보에서 해당 유저를 지워줍니다.
-    const roomID = socketRoom[socket.id];
-    console.log("86 users: ", users);
-    console.log("87 roomId: ", roomID);
-    console.log("88 users[roomID]: ", users[roomID]);
-    console.log("89 socket.id: ", socket.id);
-    if (users[roomID]) {
-      users[roomID] = users[roomID].filter((user) => user.id !== socket.id);
-      if (users[roomID].length === 0) {
-        delete users[roomID];
-        return;
+    socket.on("disconnect", () => {
+      // const roomID = socketRoom[socket.id];
+      // console.log("roomID: ", roomID);
+      // socket.leave(roomID);
+      // socket.to(roomID).emit("leave", socket.id);
+      // 방을 나가게 된다면 socketRoom과 users의 정보에서 해당 유저를 지워줍니다.
+      const roomID = socketRoom[socket.id];
+      console.log("86 users: ", users);
+      console.log("87 roomId: ", roomID);
+      console.log("88 users[roomID]: ", users[roomID]);
+      console.log("89 socket.id: ", socket.id);
+      if (users[roomID]) {
+        users[roomID] = users[roomID].filter((user) => user.id !== socket.id);
+        if (users[roomID].length === 0) {
+          delete users[roomID];
+          return;
+        }
       }
-    }
-    delete socketRoom[socket.id];
-    console.log("91 users[roomID]: ", users[roomID]);
-    console.log("98 delete socketRoom[socket.id]: ", socketRoom[socket.id]);
-    socket.broadcast.to(users[roomID]).emit("user_exit", { id: socket.id });
+      delete socketRoom[socket.id];
+      console.log("91 users[roomID]: ", users[roomID]);
+      console.log("98 delete socketRoom[socket.id]: ", socketRoom[socket.id]);
+      socket.broadcast.to(users[roomID]).emit("user_exit", { id: socket.id });
+    });
   });
 });
 
