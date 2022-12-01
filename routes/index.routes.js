a199436f9fe984b4a1906aa29f9588d9763c5217
Damiyd <dpekfa144@gmail.com>
2022-12-01 15:47:52 +0900

    feat: #14 DB에 파일 분할해서 저장  구현성공 (코드 조금 손 봐야함) 22-12-01 15:47(최예닮)

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,11 +5,13 @@ const reservationRouter = require("./reservation.routes");
 const companyRouter = require("../routes/company.routes");
 const mockInterviewRouter = require("./mockInterview.routes");
 // const room = require("./room.routes");
+const videoRouter = require("./videos.routes")
 
 router.use("/members", membersRouter);
 router.use("/interviews", reservationRouter);
 router.use("/company", companyRouter);
 router.use("/mockInterview", mockInterviewRouter);
 // router.use("/room", room);
+router.use("/video", videoRouter)
 
 module.exports = router;
