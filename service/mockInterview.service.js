3b886e66434cb548f0740d8bbd4e52c8bcc29ec0
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-12-03 21:41:50 +0900

    refactor: #13 getRandomQuestions for문 제거 22-12-03 21:41 (김민섭)

--- service/mockInterview.service.js
+++ service/mockInterview.service.js
@@ -17,14 +17,7 @@ class MockInterviewService {
       category
     );
     const shuffledQue = shuffle(questions).slice(0, number);
-    const questionArr = [];
-
-    for (let i = 0; i < shuffledQue.length; i++) {
-      questionArr.push(shuffledQue[i].question);
-    }
-
-    const data = { category, questionArr };
-
+    const data = { category, shuffledQue };
     return data;
   };
 
@@ -46,33 +39,37 @@ class MockInterviewService {
   };
 
   getInterviewResults = async (memberEmail) => {
-    const result = await this.mockInterviewRepository.getInterviewResults(memberEmail);
+    const result = await this.mockInterviewRepository.getInterviewResults(
+      memberEmail
+    );
 
     const data = [];
-    
-    for(let i = 0; i < result.length; i++) {
+
+    for (let i = 0; i < result.length; i++) {
       data.push({
         sequence: result[i].sequence,
         category: result[i].category,
         number: result[i].number,
         totalTime: result[i].totalTime,
-        createdAt: result[i].createdAt
+        createdAt: result[i].createdAt,
       });
-    };
+    }
 
     return data;
   };
 
   getInterviewResultDetails = async (sequence) => {
-    const result = await this.mockInterviewRepository.getInterviewResultDetails(sequence);
-    
+    const result = await this.mockInterviewRepository.getInterviewResultDetails(
+      sequence
+    );
+
     const data = {
-      sequence: result.sequence, 
-      category: result.category, 
-      number: result.number, 
-      totalTime: result.totalTime, 
-      resultsArr: result.result, 
-      createdAt: result.createdAt
+      sequence: result.sequence,
+      category: result.category,
+      number: result.number,
+      totalTime: result.totalTime,
+      resultsArr: result.result,
+      createdAt: result.createdAt,
     };
 
     return data;
