d1721bf72aa48d2a9c20fe8b84036aee0c3278aa
chamchimayo <chamchi.maayo@gmail.com>
2022-12-08 17:18:49 +0900

    style: #2 이메일 인증 코드 이메일 양식 수정 22-12-08 17:18(백지영)

--- service/members.service.js
+++ service/members.service.js
@@ -16,8 +16,15 @@ class MembersService {
     const mailOptions = {
       from: `"떨면 뭐하니" <${process.env.NODEMAILER_USER}>`,
       to: memberEmail,
-      subcect: "떨면 뭐하니 Auth Number",
-      text: "인증코드를 입력해주세요" + authCode,
+      subject: "[떨면 뭐하니] 인증 코드를 안내해드립니다.",
+      html: `<h3>떨면 뭐하니 인증 코드</h3>
+
+      <p>면접깡패 양성 프로그램 떨면 뭐하니에 오신 것을 환영합니다.</p>
+      <p>아래의 인증 코드를 입력하시면 가입이 정상적으로 완료됩니다</p>
+      
+      <div style="background-color:lightgray">
+          <h1>${authCode}</h1>
+      </div>`,
     };
 
     transPort.sendMail(mailOptions, function (error, info) {
@@ -63,16 +70,6 @@ class MembersService {
       gender,
       validate
     );
-    return;
-  };
-  checkDuplicatedId = async (memberEmail) => {
-    const findOneMember = await this.membersRepository.findOneMember(
-      memberEmail
-    );
-
-    if (findOneMember) {
-      throw new Error("이미 가입된 계정입니다.");
-    }
     return;
   };
 
