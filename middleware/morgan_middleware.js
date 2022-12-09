b730f3591af4f7a7f7dffc99ac9d315366a1f516
Damiyd <dpekfa144@gmail.com>
2022-12-09 14:41:09 +0900

    fix: morgan statusCode 400 -> statusCode 200 으로 변경 22-12-09 14:41(최예닮)

--- middleware/morgan_middleware.js
+++ middleware/morgan_middleware.js
@@ -20,7 +20,7 @@ const stream = {
 // 로깅 스킵 여부 (만일 배포환경이면, 코드가 400 미만라면 함수를 리턴해 버려서 로그 기록 안함. 코드가 400 이상이면 로그 기록함)
 const skip = (_, res) => {
   if (process.env.NODE_ENV === "combined") {
-    return res.statusCode < 400;
+    return res.statusCode < 200;
   }
   return false;
 };
